f3c7a73dacf6a9663c0dfced75c9d97cfee5cd49
Jon Weisz <jweisz@cs.columbia.edu>
2015-11-11 15:01:29 -0500

    Clean Python Node

--- scripts/remote_launch.py
+++ scripts/remote_launch.py
@@ -7,11 +7,11 @@ import rospy
 
 if __name__=="__main__":
     rospy.init_node('graspit_remote')
-    graspit_ssh_url = rospy.get_param('~graspit_ssh_url', 'jweisz@192.168.1.2')
+    graspit_ssh_url = rospy.get_param('~graspit_ssh_url', 'srihari@192.168.11.47')
     graspit_display = rospy.get_param('~graspit_display',':1.0')
     graspit_world_name = rospy.get_param('~graspit_world','3objectbarrett')
-    graspit_remote_bin = rospy.get_param('~graspit_remote_bin', 'graspit')
-    spargs = ['ssh',graspit_ssh_url, '-t', "pkill graspit;export DISPLAY=%s;bash -ic '%s %s'"%(graspit_display, graspit_remote_bin, graspit_world_name)]
+    graspit_remote_bin = rospy.get_param('~graspit_remote_bin', '~/graspit_bci/bin/graspit')
+    spargs = ['ssh',graspit_ssh_url, '-t', "pkill graspit;export DISPLAY=%s; export GRASPIT_REMOTE_SERVER_URL=tcp://192.168.11.200:5561; bash -ic '%s %s'"%(graspit_display, graspit_remote_bin, graspit_world_name)]
     p = subprocess.Popen(spargs)
     rospy.spin()
     p.kill()
